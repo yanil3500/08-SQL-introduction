54f906043255915c31280e2124f4a48e4d01e1e6
Sam Hamm <samhamm@gmail.com>
2017-02-15 11:35:22 -0800

    documentation instructions added

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,6 +1,6 @@
 'use strict';
 // TODO: Install and require the node postgres package into your server.js, and ensure that it's now a new dependency in your package.json
-
+// const pg = require('pg');
 const express = require('express');
 // REVIEW: Require in body-parser for post requests in our server
 const bodyParser = require('body-parser');
@@ -9,13 +9,12 @@ const app = express();
 // TODO: Complete the connection string for the url that will connect to your local postgres database
 // Windows and Linux users; You should have retained the user/pw from the pre-work for this course.
 // Your url may require that it's composed of additional information including user and password7
-const conString = ''; // NOTE: Students will have varying URLs depending on their OS
+// NOTE: Students will have varying URLs depending on their OS
+// const conString = 'postgres://localhost:5432';
 // REVIEW: Pass the conString to pg, which creates a new client object
 const client = new pg.Client(conString);
 // REVIEW: Use the client object to connect to our DB.
-client.connect(function(err) {
-  if (err) console.error(err); // Log the error if it exists.
-});
+client.connect();
 
 // REVIEW: Install the middleware plugins so that our app is aware and can use the body-parser module
 app.use(bodyParser.json());
@@ -52,8 +51,18 @@ app.get('/articles', function(request, response) {
 
 app.post('/articles', function(request, response) {
   client.query(
-    ``, // TODO: Write the SQL query to insert a new record
-    [] // TODO: Get each value from the request's body
+    `INSERT INTO
+    articles(title, author, "authorUrl", category, "publishedOn", body)
+    VALUES ($1, $2, $3, $4, $5, $6);
+    `, // DONE: (above) Write the SQL query to insert a new record
+    [
+      request.body.title,
+      request.body.author,
+      request.body.authorUrl,
+      request.body.category,
+      request.body.publishedOn,
+      request.body.body
+    ] // DONE: (above) Get each value from the request's body
   );
   response.send('insert complete');
 });
